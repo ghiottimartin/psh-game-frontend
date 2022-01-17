e0f15b4414a13787778ecff3f350ad47a81a3a83
martingh15 <martinghiotti@yahoo.com.ar>
2022-01-17 18:11:51 -0300

    Design of csv button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,9 @@ function Nav() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <a className="navbar-brand" href="#">PSH Game</a>
+            <form className="form-inline">
+                <button className="btn btn-outline-success" type="button">CSV</button>
+            </form>
         </nav>
     )
 }
